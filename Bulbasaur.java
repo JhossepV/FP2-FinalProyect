9c4a5cc297d6d00b33a5b4fba2a09d78d0e104ed
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2023-12-20 11:55:42 -0500

    Idle animation: tried 1

--- Bulbasaur.java
+++ Bulbasaur.java
@@ -1,12 +1,29 @@
-public class Bulbasaur extends Pokemon {
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.Image;
 
+public class Bulbasaur extends Pokemon {
+    
+    private ArrayList<Image> animationFrames;
+    private int currentFrameIndex;
     public Bulbasaur() {
         super("Bulbasaur", 5, 100);
+        loadAnimationFrames();
     }
 
     @Override
     public void attack() {
         System.out.println(getName() + " useo Latigo Cepa!");
+    }
+    private void loadAnimationFrames(){
+        animationFrames = new ArrayList<>();
+        for(int i = 1; i <= 3; i++){
+            String frameFileName = "Images/bulbasaur"+i+".gif";
+            Image frame = new ImageIcon(frameFileName).getImage();
+            animationFrames.add(frame);
+        }
+
     }
 
 }
